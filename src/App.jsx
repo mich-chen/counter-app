725a61e956832cb9bec507fb73f30009c2752e57
Michelle Chen <mich.chen.94@gmail.com>
2023-10-13 00:30:19 -0700

    add increment, decrement, reset, log handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [countLog, setCountLog] = useState([]);
+
+  const handleIncrement = () => {
+    const logDescription = `Increment at ${new Date(Date.now()).toUTCString()}`;
+    setCount(count + 1);
+    setCountLog([...countLog, logDescription]);
+  }
+
+  const handleDecrement = () => {
+    const logDescription = `Decrement at ${new Date(Date.now()).toUTCString()}`;
+    setCount(count - 1);
+    setCountLog([...countLog, logDescription]);
+  }
+
+  const handleReset = () => {
+    setCount(0);
+    setCountLog([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className='count-container'>
+        <h1>Counter App</h1>
+        <h2>{count}</h2>
+
+        <div className='button-container'>
+          <button onClick={handleIncrement}>Increment</button>
+          <button onClick={handleDecrement}>Decrement</button>
+        </div>
+
+        <reset-container>
+          <button onClick={handleReset}>Reset</button>
+        </reset-container>
+        
+
+      </div>
+
+      <div className='log-container'>
+        <h2>Count Log</h2>
+        <>
+          {countLog 
+            ? countLog.map((log, index) => <li key={index}>{log}</li>)
+            : 'No Logs to Display'}
+        </>
+
+      </div>
+      
     </div>
   );
 }
